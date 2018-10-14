a5101db9b84628261110282d2c3709e15229d673
scottasc <scottschulfer@gmail.com>
2018-10-14 16:58:30 -0500

    pagespeed chrome extension

---
+++ popup.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      var f = d.createElement('form');
+      f.target = '_blank';
+      f.action = 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed';
+      f.method = 'get';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab.url;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  }, false);
+}, false);
